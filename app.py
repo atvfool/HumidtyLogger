5fdd4d8dea1e91cf15e85c979165d067ce0e3984
Andrew Hayden <atvfool@gmail.com>
2023-03-18 09:36:02 -0400

    Adds table and quick write

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 from flask import Flask, render_template
+import datetime
 
 app = Flask(__name__)
 
@@ -9,7 +10,18 @@ def index():
     data = []
     for line in lineData:
         data.append(line.split(","))
+    f.close()
     return render_template("index.html", data=data);
+
+@app.route("/log/<location>/<humidity>/<temp>")
+def log(location, humidity, temp):
+    currentDate = datetime.datetime.now()
+    f = open("results.csv", "a")
+    newLine = f"{currentDate},{humidity},{location},{location}\n"
+    f.write(newLine)
+    f.close()
+    app.redirect("/", 200)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
