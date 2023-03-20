eb440b85e0ecf00de786b3c2481f0ba63a86374c
atvfool <atvfool@gmail.com>
2023-03-20 13:10:22 -0400

    Adds lil security, not meant for serious applications, just running locally

--- WebApp/views.py
+++ WebApp/views.py
@@ -1,17 +1,21 @@
-from flask import Blueprint, render_template, redirect, url_for
+from flask import Blueprint, render_template, redirect, url_for, abort, request
 import datetime
+import secret1234
 
 views = Blueprint(__name__, "views")
 
-@views.route("/")
-def home():
-    f = open("results.csv", "r")
-    lineData = f.readlines()
-    data = []
-    for line in lineData:
-        data.append(line.split(","))
-    f.close()
-    return render_template("index.html", data=data)
+@views.route("/<key>")
+def home(key):
+    if key != secret1234.presharedkey:
+        abort(401)
+    else:
+        f = open("results.csv", "r")
+        lineData = f.readlines()
+        data = []
+        for line in lineData:
+            data.append(line.split(","))
+        f.close()
+        return render_template("index.html", data=data)
 
 @views.route("/log/<location>/<humidity>/<temp>")
 def log(location, humidity, temp):
